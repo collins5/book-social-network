2e220f84b9683369e50adfaf62e7c39953ed935f
Collins Audrey Souop Tagne <t.stacdev@gmail.com>
2024-04-23 08:40:36 +0200

    Token Entity and Token Repository done

---
+++ book-network/src/main/java/de/stacdev/booknetwork/tokens/Token.java
@@ -0,0 +1,39 @@
+package de.stacdev.booknetwork.tokens;
+
+import de.stacdev.booknetwork.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Collins Souop
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatesAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userid",
+            nullable = false)
+    private User user;
+}
